02f2875a0900b0c1a4a736761cda2655c236b715
Jimmy De Los Angeles <jimmydelosangeles@gmail.com>
2017-03-24 17:49:52 -0400

    socket.oi and server running

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
-var express = require('express');
-var http = require('http')
-var socketio = require('socket.io');
+// const express = require('express');
+// const http = require('http')
+// const socketio = require('socket.io');
+//
+// const app = express();
+// const server = http.Server(app);
+// const websocket = socketio(server);
+// server.listen(3000, () => console.log('listening on *:3000'));
+//
+// // The event will be called when a client is connected.
+// websocket.on('connection', (socket) => {
+//   console.log('A client just joined on', socket.id);
+// });
 
-var app = express();
-var server = http.Server(app);
-var websocket = socketio(server);
-server.listen(3000, () => console.log('listening on *:3000'));
+
+
+const express = require('express');
+const app     = express();
+const server  = require('http').Server(app);
+const io      = require('socket.io')(server);
+const port    = process.env.PORT || 3000;
+
+server.listen(3000);
+
 
 // The event will be called when a client is connected.
-websocket.on('connection', (socket) => {
-  console.log('A client just joined on', socket.id);
+io.on('connection', (socket) => {
+  console.log("we have a connection @ " + socket.id);
+  // socket.on("new-message", (msg) => {
+  //   console.log(msg);
+  //   io.emit("receive-message", msg);
+  // })
 });
+
+
+// Once the server is online
+server.listen(port, ()=>console.log("server is running @ ", port));
