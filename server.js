4c6e52515e5a6d7c0ea6c94a7a8102c01eaa9e95
Christian Pena <christianpena9@gmail.com>
2017-03-26 14:07:02 -0400

    cleaning up unwanted comments

--- server.js
+++ server.js
@@ -1,19 +1,3 @@
-// const express = require('express');
-// const http = require('http')
-// const socketio = require('socket.io');
-//
-// const app = express();
-// const server = http.Server(app);
-// const websocket = socketio(server);
-// server.listen(3000, () => console.log('listening on *:3000'));
-//
-// // The event will be called when a client is connected.
-// websocket.on('connection', (socket) => {
-//   console.log('A client just joined on', socket.id);
-// });
-
-
-
 const express = require('express');
 const app     = express();
 const server  = require('http').Server(app);
@@ -26,10 +10,7 @@ server.listen(3000);
 // The event will be called when a client is connected.
 io.on('connection', (socket) => {
   console.log("we have a connection @ " + socket.id);
-  // socket.on("new-message", (msg) => {
-  //   console.log(msg);
-  //   io.emit("receive-message", msg);
-  // })
+  
 });
 
 
