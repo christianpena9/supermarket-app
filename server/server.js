8dd8975424ca788276050c080a4393344ded79c7
Jimmy De Los Angeles <jimmydelosangeles@gmail.com>
2017-03-29 01:59:59 -0400

    back online with socket id

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app     = express();
+const server  = require('http').Server(app);
+const io      = require('socket.io')(server);
+const port    = process.env.PORT || 3000;
+
+
+// The event will be called when a client is connected.
+io.on('connection', (socket) => {
+  console.log("we have a new connection @ " + socket.id);
+
+});
+
+
+// Once the server is online
+server.listen(port, ()=>console.log("server is running @ ", port));
