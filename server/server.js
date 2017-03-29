6e2800825590683b4aff118a86176ffcf072fa00
Jimmy De Los Angeles <jimmydelosangeles@gmail.com>
2017-03-29 03:15:10 -0400

    server working

--- server/server.js
+++ server/server.js
@@ -6,11 +6,19 @@ const port    = process.env.PORT || 3000;
 
 
 // The event will be called when a client is connected.
-io.on('connection', (socket) => {
-  console.log("we have a new connection @ " + socket.id);
+// io.on('connection', (socket) => {
+//   console.log("we have a new connection @ " + socket.id);
+//   socket.emit('hello', 'can you hear me?', 1, 2, 'abc');
+//
+// });
 
+
+io.on('connection', function (socket) {
+    console.log("we have a new connection @ " + socket.id);
+    socket.on('message', function (data) {
+        socket.broadcast.emit('message', data);
+    });
 });
-
 
 // Once the server is online
 server.listen(port, ()=>console.log("server is running @ ", port));
