c84332929cac800efb9d5acbf00f31d779b4210c
Christian Pena <christianpena9@gmail.com>
2017-04-04 17:35:46 -0400

    added navigation. Still missing receiver component to send data back to main app

--- index.android.js
+++ index.android.js
@@ -17,120 +17,147 @@ import {
   TextInput,
   TouchableOpacity
 } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 import "./UserAgent";
 import io from "socket.io-client/dist/socket.io";
+
+/* CUSTOM IMPORT STYLES BELOW */
+import { styles } from './styles/mainStyle';
 
 const onButtonPress = () => {
     Alert.alert('Button was pressed!');
 };
 
-export default class SuperMarketApp extends Component {
-  constructor(){
-    super();
-    //has to listen to localhost but with actual IP Address
-    this.socket = io('http://192.168.0.6:3000', {jsonp: false});
-    this.state = {
-      isSwitchOn:false,
-      text: "enter color",
-      incomingText: "waiting",
-      backColor: "pink"
+export default class HomeScreen extends Component {
+    constructor() {
+        super();
+        //has to listen to localhost but with actual IP Address
+        this.socket = io('http://172.28.45.126:3000', {jsonp: false});
+        this.state = {
+            isSwitchOn:false,
+            text: "enter color",
+            incomingText: "waiting",
+            backColor: "pink"
+        }
+        console.log(this.socket);
+
+        //data comes back and you can use it for anything
+        this.socket.on("server-send", (data)=> {
+            this.setState({ backColor: data });
+            this.setState({ incomingText: data });
+        });
     }
-    console.log(this.socket);
 
-    //data comes back and you can use it for anything
-    this.socket.on("server-send", (data)=> {
-      this.setState({ backColor: data });
-      this.setState({ incomingText: data });
-    });
-  }
+    handleChange(event) {
+        this.setState({
+            text: event.nativeEvent.text
+        });
+    }
 
-  handleChange(event){
-    this.setState({
-      text: event.nativeEvent.text
-    });
-  }
+    sendMe() {
+        this.socket.emit("client-send", this.state.text);
+    }
 
-  sendMe(){
-    this.socket.emit("client-send", this.state.text);
-  }
-
-  render() {
-    return (
-      // did inline styling to test incoming socket data
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center',
+    render() {
+        const { navigate } = this.props.navigation;
+        return (
+            // did inline styling to test incoming socket data
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center',
 backgroundColor: this.state.backColor}}>
 
-        <Text style={styles.text}>
-          ARE YOU AVALIABLE?
-        </Text>
+                <Text style={styles.text}>
+                    ARE YOU AVALIABLE?
+                </Text>
 
-        <Switch
-          onValueChange={(value) => this.setState({isSwitchOn: value})}
-          value= {this.state.isSwitchOn}
-         />
+                <TouchableOpacity
+                    onPress = {() => navigate('ReceiverScreen')}
+                    style = {styles.touch}>
+                    <Text style={styles.sendText}>Receiver</Text>
+                </TouchableOpacity>
 
-        <Button
-            onPress={onButtonPress}
-            title="Call!"
-            color="#841585"
-            disabled={!this.state.isSwitchOn}
-        />
+                <TouchableOpacity
+                    onPress = {() => navigate('ClientScreen', { isSwitchOn: this.state.isSwitchOn })}
+                    style = {styles.touch}>
+                    <Text style={styles.sendText}>Client</Text>
+                </TouchableOpacity>
 
-        <TextInput
-          style={styles.input}
-          value={this.state.text}
-          // onChangeText={(text)=> this.setState({text})}
-          onChange={this.handleChange.bind(this)}
-        />
+                <TextInput
+                    style={styles.input}
+                    value={this.state.text}
+                    // onChangeText={(text)=> this.setState({text})}
+                    onChange={this.handleChange.bind(this)}
+                />
 
-        <TouchableOpacity
-          onPress={()=> {this.sendMe()}}
-          style={styles.touch}>
-          <Text style={styles.sendText}>Send</Text>
-        </TouchableOpacity>
+                <TouchableOpacity
+                    onPress = {() => {this.sendMe()}}
+                    style = {styles.touch}>
+                    <Text style={styles.sendText}>Send</Text>
+                </TouchableOpacity>
 
-        <Text style={styles.text}>
-          ServerSaid--->{this.state.incomingText}
-        </Text>
-      </View>
-    );
-  }
+                <Text style={styles.text}>
+                    ServerSaid--->{this.state.incomingText}
+                </Text>
+            </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-    view: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'green'
-    },
-    text: {
-        fontSize: 50,
-        color: 'red'
-    },
-    sendText: {
-      textAlign: "center",
-      fontSize: 25,
-      color: "white"
-    },
-    button: {
-        width: 500,
-        height: 500
-    },
-    input:{
-      width: 400,
-      fontSize: 50,
-      color: 'yellow'
-    },
-    touch:{
-      width: 100,
-      height: 50,
-      borderColor: "white",
-      borderWidth: 3,
-      backgroundColor: "rgb(0,165,255)"
+/* RECEIVER SCREEN BELOW */
+class ReceiverScreen extends React.Component {
+    render() {
+        return(
+            <View style={styles.view}>
+                <Text>Receiver Screen</Text>
+            </View>
+        );
     }
+}
 
+/* CLIENT SCREEN BELOW */
+class ClientScreen extends React.Component {
+    render() {
+        const { params } = this.props.navigation.state;
+        return(
+            <View style={styles.view}>
+                <Button
+                    title = 'Call Now!'
+                    onPress = {onButtonPress}
+                    disabled = { !params.isSwitchOn }
+                />
+            </View>
+        );
+    }
+}
 
+/* CUSTOM ROUTE BELOW */
+const SuperMarketApp = StackNavigator({
+    Home: {
+        screen: HomeScreen,
+        navigationOptions: {
+            title: 'Welcome',
+            header: {
+                visible: false
+            }
+        }
+    },
+    ClientScreen: {
+        screen: ClientScreen,
+        navigationOptions: {
+            title: 'Client Screen',
+            header: {
+                visible: false
+            }
+        }
+    },
+    ReceiverScreen: {
+        screen: ReceiverScreen,
+        navigationOptions: {
+            title: 'Receiver Screen',
+            header: {
+                visible: false
+            }
+        }
+    }
 });
 
 AppRegistry.registerComponent('SuperMarketApp', () => SuperMarketApp);
---
+++ styles/mainStyle.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    view: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'green'
+    },
+    text: {
+        fontSize: 50,
+        color: 'red'
+    },
+    sendText: {
+        textAlign: 'center',
+        fontSize: 25,
+        color: 'white'
+    },
+    button: {
+        width: 500,
+        height: 500
+    },
+    input: {
+        width: 400,
+        fontSize: 50,
+        color: 'yellow'
+    },
+    touch: {
+        width: 110,
+        height: 50,
+        borderColor: 'white',
+        borderWidth: 3,
+        backgroundColor: 'rgb(0,165,255)'
+    }
+});
